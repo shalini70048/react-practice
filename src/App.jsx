cbd71e56428c610b162a203db0d487596e44f84d
shalini70048 <shalini70048@gmail.com>
2025-05-13 01:20:20 +0530

    add input field

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
   const[name, setName] =useState("shalu")
+  const[typeWriter, setTypeWriter] = useState("19811521");
 
   const incremenBtn = () =>{
     setCount(count+1)
   }
   const decrementBtn = () =>{
-    setCount(count-1)
+    setCount((prevCount)=>{
+      if(prevCount <= 0){
+        return 0;
+      }
+      else{
+        setCount(prevCount-1)
+      }
+    })
   }
   const updateName =() =>{
     // setName((prevName) =>{
@@ -33,7 +41,22 @@ function App() {
 
       <h1>Name : {name}</h1>
       <button onClick={updateName} >Update Name</button>
+
+      <input
+        type="text"
+        value={typeWriter}
+        placeholder='enter your name'
+        onChange={(event) =>{
+          setTypeWriter(event.target.value);
+        }
+
+        }
+      />
+      <div>
+        {typeWriter}
+      </div>
     </div>
+
   )
 }
 
